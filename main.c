4840de3eb3232e8551422490041e0c86da31c4be
Muhesena Nasiha Syeda <smuhesena@gmail.com>
2021-09-21 08:22:17 +0600

    assigning fileloader to romi

--- main.c
+++ main.c
@@ -364,7 +364,7 @@ psmenu2:
     }  ///end get product choice
 }
 
-void fileloader()//EHASAN
+void fileloader()//RAOMI
 {
 	int serial, qntt; float prc; char itm[25];
 	FILE *admin;
@@ -569,9 +569,9 @@ void item_list()//RAOMI
     cls();
     printf("press .0. Main Menu ");
     linebreak(4);
-	  int quan, dat;
-	  float pri;
-	  char itemnm[40];
+	int quan, dat;
+	float pri;
+	char itemnm[40];
     node *temp;
     temp = list;
 
